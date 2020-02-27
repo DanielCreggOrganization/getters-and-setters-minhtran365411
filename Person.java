b3f424cdd5eec325c58e9cb676c2d443279ab75a
minhtran365411 <g00365411@gmit.ie>
2020-02-27 13:48:02 +0000

    person

--- Person.java
+++ Person.java
@@ -1,5 +1,21 @@
 class Person {
     // Instance Variables
-    
+    private String firstName;
+    private String middleName;
+    private String userName;
+
     // Methods
+    //Setter for Name
+    public void setUser(String fName, String mName, String uName) {
+        firstName = fName;
+        middleName = mName;
+        userName = uName;
+    }
+    //Getter for Name
+    public String getUser() {
+        String user = firstName + " " + middleName + ": " + userName;
+        return user;
+    }
+
+
 }
