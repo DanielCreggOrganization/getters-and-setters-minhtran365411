36ef62fbff734d56f267042d0b3d5030bf1dc4f4
minhtran365411 <g00365411@gmit.ie>
2020-02-27 13:49:51 +0000

    tt

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
 public class Main {
     // Main method
     public static void main(String[] args) {
-        Person userObj1 = new Person();
-        //calling setter
-        userObj1.setUser("Minh","Tran", "minhtran365411");
-        //calling getter
-        System.out.println(userObj1.getUser());
+        //create a person
+            Person userObj1 = new Person();
+            //calling setter
+            userObj1.setUser("Minh","Tran", "minhtran365411");
+            //calling getter
+            System.out.println(userObj1.getUser());
+
+        //create a person
+            Person userObj2 = new Person();
+            //calling setter
+            userObj2.setUser("Meryl","Streep", "gorgeousMeryl");
+            //calling getter
+            System.out.println(userObj2.getUser());
     }
 }
